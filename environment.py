ceee352906295ac04a61bae8d74d16407c96cdfd
makramchahine <makramchahine8@gmail.com>
2024-08-06 15:03:14 -0400

    add MIT letters to obstacles

--- environment.py
+++ environment.py
@@ -83,13 +83,69 @@ class Environment():
 
 		return obstacle
 
+	def make_obstacles_I(self, initial_point):
+		"""
+		Given a initial point, it makes a obstacle with shape of I.
+
+		Parameters
+		----------
+		initial_point : tuple
+			X and Y coordinates, starting from the top-left most part where
+			the obstacle will be placed.
+
+		Returns
+		-------
+		list
+			A collection of sides composing the I obstacle.
+		"""
+		x, y = initial_point[0], initial_point[1]
+		width, height = 50, 150
+
+		side1 = pygame.Rect(x, y, width, height)
+
+		obstacle = [side1]
+
+		return obstacle
+
+	def make_obstacles_M(self, initial_point):
+		"""
+		Given a initial point, it makes a obstacle with shape of M.
+
+		Parameters
+		----------
+		initial_point : tuple
+			X and Y coordinates, starting from the top-left most part where
+			the obstacle will be placed.
+
+		Returns
+		-------
+		list
+			A collection of sides composing the M obstacle.
+		"""
+		x, y = initial_point[0], initial_point[1]
+		width, height = 150, 150
+
+		# Create rectangles for the letter 'M'
+		side1 = pygame.Rect(x, y, width // 4, height)  # Left vertical line
+		side2 = pygame.Rect(x + width // 2 - width // 12, y, width // 4, height)  # Middle vertical line
+		side3 = pygame.Rect(x + width - width // 6, y, width // 4, height)  # Right vertical line
+		side4 = pygame.Rect(x + width // 6, y , width - width // 3,
+							height // 4)  # Middle horizontal line
+
+		obstacle = [side1, side2, side3, side4]
+
+		return obstacle
+
 	def make_obstacles(self):
-		"""Generate the obstacles to be placed on the final map."""
-		obstacle1 = self.make_obstacles_T(initial_point=(350, 200))
-		obstacle2 = self.make_obstacles_L(initial_point=(150, 20))
+		"""Generate the obstacles to be placed on the final map to read MIT."""
+		obstacle1 = self.make_obstacles_M(initial_point=(75 , 150))
+		obstacle2 = self.make_obstacles_I(initial_point=(300, 150))
+		obstacle3 = self.make_obstacles_T(initial_point=(425, 150))
+
 
 		self.obstacles.append(obstacle1)
 		self.obstacles.append(obstacle2)
+		self.obstacles.append(obstacle3)
 
 		return self.obstacles
 
